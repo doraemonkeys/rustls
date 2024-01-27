ca19024243c839a368011997b542ef0547a34905
doraemon <doraemonkey@qq.com>
2024-01-27 19:32:49 +0800

    use pki-types crate from the fork repo

--- rustls/src/msgs/handshake.rs
+++ rustls/src/msgs/handshake.rs
@@ -232,7 +232,7 @@ impl ServerNamePayload {
                     "Illegal SNI hostname received {:?}",
                     String::from_utf8_lossy(&raw.0)
                 );
-                Ok(Self::HostName(DnsName("".to_string())))
+                Ok(Self::HostName(DnsName::from_empty()))
             }
         }
     }
